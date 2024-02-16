feeb221ece5bfd0bef1c2dcec917bc4e0d15e00b
Michell <49607391+rawello@users.noreply.github.com>
2024-02-16 19:11:46 +0400

    init

---
+++ pp.js
@@ -0,0 +1,171 @@
+const btnEasy = document.getElementById('easy');
+const btnMed = document.getElementById('medium');
+const btnHard = document.getElementById('hard');
+
+const canvas = document.getElementById('gameCanvas');
+const ctx = canvas.getContext('2d');
+
+let compSpeed = 1.75;
+let ballSpeed = 5.0;
+
+let player = {
+    x: 20,
+    y: canvas.height / 2 - 50,
+    width: 10,
+    height: 100,
+    speed: 5,
+    score: 0
+};
+
+let computer = {
+    x: canvas.width - 30,
+    y: canvas.height / 2 - 50,
+    width: 10,
+    height: 100,
+    speed: compSpeed,
+    score: 0
+};
+
+let ball = {
+    x: canvas.width / 2,
+    y: canvas.height / 2,
+    radius: 10,
+    speed: 5,
+    velocityX: 5,
+    velocityY: 5
+};
+
+function drawRect(x, y, w, h, color) {
+    ctx.fillStyle = color;
+    ctx.fillRect(x, y, w, h);
+}
+
+function drawCircle(x, y, r, color) {
+    ctx.fillStyle = color;
+    ctx.beginPath();
+    ctx.arc(x, y, r, 0, Math.PI * 2, false);
+    ctx.closePath();
+    ctx.fill();
+}
+
+function drawText(text, x, y, color) {
+    ctx.fillStyle = color;
+    ctx.font = '45px Arial';
+    ctx.fillText(text, x, y);
+}
+
+function render() {
+    drawRect(0, 0, canvas.width, canvas.height, 'black');
+    drawRect(player.x, player.y, player.width, player.height, 'white');
+    drawRect(computer.x, computer.y, computer.width, computer.height, 'white');
+    drawCircle(ball.x, ball.y, ball.radius, 'white');
+    drawText(player.score, canvas.width / 4, 100, 'white');
+    drawText(computer.score, (3 * canvas.width) / 4, 100, 'white');
+}
+
+function collision(b, p) {
+    p.top = p.y;
+    p.bottom = p.y + p.height;
+    p.left = p.x;
+    p.right = p.x + p.width;
+
+    b.top = b.y - b.radius;
+    b.bottom = b.y + b.radius;
+    b.left = b.x - b.radius;
+    b.right = b.x + b.radius;
+
+    return p.left < b.right && p.top < b.bottom && p.right > b.left && p.bottom > b.top;
+}
+
+function resetBall() {
+    ball.x = canvas.width / 2;
+    ball.y = canvas.height / 2;
+    ball.speed = ball.speed;
+    ball.velocityX = -ball.velocityX;
+}
+
+function computerMovement() {
+    let center = computer.y + computer.height / 2;
+
+    if (center < ball.y - 35) {
+        computer.y += computer.speed;
+    } else if (center > ball.y + 35) {
+        computer.y -= computer.speed;
+    }
+}
+
+function game() {
+    update();
+    render();
+}
+
+function update() {
+    btnEasy.addEventListener('click', function () {
+        computer.speed = 1.75;
+        ball.speed = 5.0;
+    })
+
+    btnMed.addEventListener('click', function () {
+        computer.speed = 2.75;
+        ball.speed = 6.5;
+    })
+
+    btnHard.addEventListener('click', function () {
+        computer.speed = 4.75;
+        ball.speed = 8;
+    })
+
+    ball.x += ball.velocityX;
+    ball.y += ball.velocityY;
+
+    computerMovement();
+
+    if (ball.y - ball.radius < 0 || ball.y + ball.radius > canvas.height) {
+        ball.velocityY = -ball.velocityY;
+    }
+
+    let playerPaddle = (ball.x - ball.radius < player.x + player.width) &&
+        (player.y < ball.y + ball.radius && player.y + player.height > ball.y - ball.radius);
+
+    let computerPaddle = (ball.x + ball.radius > computer.x) &&
+        (computer.y < ball.y + ball.radius && computer.y + computer.height > ball.y - ball.radius);
+
+    if (ball.x - ball.radius < 0) {
+        computer.score++;
+        resetBall();
+    } else if (ball.x + ball.radius > canvas.width) {
+        player.score++;
+        resetBall();
+    }
+
+    if (playerPaddle) {
+        let collisionPoint = (ball.y - (player.y + player.height / 2));
+        collisionPoint = collisionPoint / (player.height / 2);
+
+        let angle = (collisionPoint * Math.PI) / 4;
+
+        ball.velocityX = ball.speed * Math.cos(angle);
+        ball.velocityY = ball.speed * Math.sin(angle);
+
+        ball.speed += 0.025;
+        computer.speed += 0.005
+    } else if (computerPaddle) {
+        let collisionPoint = (ball.y - (computer.y + computer.height / 2));
+        collisionPoint = collisionPoint / (computer.height / 2);
+
+        let angle = (collisionPoint * Math.PI) / 4;
+
+        ball.velocityX = -ball.speed * Math.cos(angle);
+        ball.velocityY = ball.speed * Math.sin(angle);
+
+        ball.speed += 0.1;
+    }
+}
+
+canvas.addEventListener('mousemove', function (e) {
+    let rect = canvas.getBoundingClientRect();
+    player.y = e.clientY - rect.top - player.height / 2;
+});
+
+
+setInterval(game, 1000 / 60);
